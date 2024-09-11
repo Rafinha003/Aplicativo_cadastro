24ee0287755070be5e792f7899b1d2ead471fefd
android3 <rafael.nascimento@sou.unaerp.edu.br>
2024-09-10 21:34:39 -0300

    Criando tela de cadastro de usuario, com sua funcionalidade

--- app/src/main/java/com/example/aplicativodecadastro/MainActivity.kt
+++ app/src/main/java/com/example/aplicativodecadastro/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.aplicativodecadastro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,19 +15,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val editTextName = findViewById<EditText>(R.id.editTextName)
+        val editTextEmail = findViewById<EditText>(R.id.editTextEmail)
         val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
         val buttonLogin = findViewById<Button>(R.id.buttonLogin)
         val buttonRegister = findViewById<Button>(R.id.buttonRegister)
 
+        val usuarioCadastrado = intent.getStringExtra("nomeUsuario")
+        val senhaCadastrado = intent.getStringExtra("senhaUsuario")
+        val emailCadastrado = intent.getStringExtra("emailUsuario")
+
         buttonLogin.setOnClickListener {
             val username = editTextName.text.toString()
             val password = editTextPassword.text.toString()
+            val email = editTextEmail.text.toString()
 
-            if (username == "rafael" && password == "123") {
-                Toast.makeText(this, "Bem-vindo, Rafael!", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Esse usuário não existe. Por favor, crie uma conta.", Toast.LENGTH_SHORT).show()
+            if ( username == usuarioCadastrado && password == senhaCadastrado && email == emailCadastrado) {
+                Toast.makeText(this, "Bem-vindo, $usuarioCadastrado!", Toast.LENGTH_SHORT).show()
+            } else if(username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+                Toast.makeText(this, "Digite todas as informações para login", Toast.LENGTH_SHORT).show()
+            } else{
+                Toast.makeText(this, "Digite um usuário existe", Toast.LENGTH_SHORT).show()
             }
         }
+
+        buttonRegister.setOnClickListener {
+            val intent = Intent(this, TelaCadsatro::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/aplicativodecadastro/TelaCadsatro.kt
@@ -0,0 +1,50 @@
+package com.example.aplicativodecadastro
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Button
+import android.widget.Toast
+
+class TelaCadsatro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_cadsatro)
+
+        val nomeusuario = findViewById<EditText>(R.id.CriarNomeUsuario)
+        val emailUsuario = findViewById<EditText>(R.id.ColoqueEmail)
+        val senhaUsuario = findViewById<EditText>(R.id.CriarSenha)
+        val confirmaSenhaUsuario = findViewById<EditText>(R.id.ConfirmaSenha)
+        val buttonVoltar = findViewById<Button>(R.id.ButtonVoltar)
+        val buttonCadastrar = findViewById<Button>(R.id.ButtonCadastrarConta)
+
+        buttonVoltar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonCadastrar.setOnClickListener {
+            val valorNome = nomeusuario.text.toString()
+            val valorSenha = senhaUsuario.text.toString()
+            val valorEmail = emailUsuario.text.toString()
+            val confirmaSenha = confirmaSenhaUsuario.text.toString()
+
+            if(valorSenha != confirmaSenha){
+                Toast.makeText(this, "As senhas não dos campos não são iguais", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener;
+            }
+
+            if(valorNome.isEmpty() || valorSenha.isEmpty() || valorEmail.isEmpty() || confirmaSenha.isEmpty()){
+                Toast.makeText(this, "Existe campos que ainda não foram preenchido", Toast.LENGTH_SHORT).show()
+            }else{
+
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("nomeUsuario", valorNome)
+                intent.putExtra("senhaUsuario", valorSenha)
+                intent.putExtra("emailUsuario", valorEmail)
+                startActivity(intent)
+            }
+        }
+    }
+}
